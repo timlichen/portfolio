fcd40c1e1ebbf4180ff62e3448f0dd5138c828d7
timlichen <tim.li.chen@gmail.com>
2016-01-19 19:00:49 -0800

    package cleanup

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 app.use(express.static( __dirname + '/public'));
 
-require('./server/config/routes.js')(app);
+
 
 app.listen(8000, function(){
   console.log("Listening on port 8000");
