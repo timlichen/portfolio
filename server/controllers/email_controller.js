0b230426723d3be4d18ff027a38fdbc886906e91
timlichen <tim.li.chen@gmail.com>
2016-01-24 13:30:03 -0800

    email controller cleaned

--- server/controllers/email_controller.js
+++ server/controllers/email_controller.js
@@ -1,15 +1,6 @@
 var nodemailer = require("nodemailer");
 var mandrillTransport = require('nodemailer-mandrill-transport');
 
-
-// var smtpTransport = nodemailer.createTransport("SMTP",{
-//     service: "Mandrill",
-//     auth: {
-//         user: "tim.li.chen@gmail.com",
-//         pass: "sR3RRKyZJIar9LLns-D38Q"
-//     }
-// });
-
 var transport = nodemailer.createTransport(mandrillTransport({
   auth: {
     apiKey: 'sR3RRKyZJIar9LLns-D38Q'
@@ -36,22 +27,6 @@ module.exports = (function() {
           }
         });
       }
-  } //END send_email
-
-    //   var mailOptions={
-    //      to : req.body.to,
-    //      subject : req.body.subject,
-    //      text : req.body.text + " " +req.body.from
-    //   }
-    //   console.log(mailOptions);
-    //     smtpTransport.sendMail(mailOptions, function(error, response){
-    //       if(error){
-    //         console.log(error);
-    //         res.end("error");
-    //       }else{
-    //         console.log("Message sent: " + response.message);
-    //         res.end("sent");
-    //       }
-    //   });
+    } //END send_email
   } // END return
 })(); // END module.exports function
