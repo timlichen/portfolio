c3568b22ea1321fe7cf85401fe01890ecc53e5a3
timlichen <tim.li.chen@gmail.com>
2016-01-24 16:34:00 -0800

    validation change

--- public/lib/fact_ctrl.js
+++ public/lib/fact_ctrl.js
@@ -4,13 +4,16 @@ var portfolio_app = angular.module('portfolio_app', ['ngRoute']);
 portfolio_app.factory('portfolio_Factory', function($http){
   var factory = {};
     factory.sendEmail = function(emailData, callback){
-      console.log(emailData.from);
+      // console.log(emailData.from);
 
 if(emailData == undefined || !emailData.text){
+      data = "Uhoh, something is wrong!";
+      callback(data);
       console.log("no");
     } else {
         $http.post('/email', emailData)
           .success(function(data, status) {
+            data = "Success, message sent!"
             callback(data);
         })
       }
@@ -24,7 +27,7 @@ portfolio_app.controller('portfolio_app', function ($scope, portfolio_Factory){
 $scope.submitEmail = function() {
       portfolio_Factory.sendEmail($scope.email, function(data){
         console.log(data);
-        $scope.success = "Success, message sent!";
+        $scope.success = data;
       });
   };
 })
